d0f01cef4380f8a88f7cf7bb1d544725dfdf1eca
bylane <juanaubone1234@gmail.com>
2021-06-17 17:40:43 -0300

    Fix - IS NOT NULL

--- add_user.sql
+++ add_user.sql
@@ -1,4 +1,4 @@
-CREATE FUNCTION `add_user`(_name VARCHAR(250), _email VARCHAR(250), _password TEXT, _confirmPassword TEXT) RETURNS TEXT
+CREATE DEFINER=`root`@`localhost` FUNCTION `add_user`(_name VARCHAR(250), _email VARCHAR(250), _password TEXT, _confirmPassword TEXT) RETURNS text CHARSET utf8
 BEGIN
 	
     DECLARE _message TEXT;
@@ -7,20 +7,20 @@ BEGIN
 	-- validacion
 		
         -- Validar nombre
-		IF (_name <> null AND _name <> '') THEN
+		IF (_name IS NOT null AND _name <> '') THEN
         
         
             -- Validar email
-            IF (_email <> null AND _email <> '') THEN
-				IF () THEN -- USAR LOCATE
+            IF (_email IS NOT null AND _email <> '') THEN
+				IF (LOCATE('@', _email)) THEN -- USAR LOCATE
 					IF NOT EXISTS (SELECT * from `list_user` WHERE (email_user =_email)) THEN -- VERIFICAR QUE NO EXISTA
                 
                 
 						-- Validar password
-						IF (_password <> null AND _password <> '') THEN
+						IF (_password IS NOT null AND _password <> '') THEN
 							IF (_password = _confirmPassword ) THEN -- COMPARAR QUE EL PASSWORD SEA IGUAL
 								
-								INSERT INTO `list_user` (`name_user`, `email_user`,`password_user`) VALUES (_name, _email ,md5(_password) ) -- INSERTAR DATOS Y GUARDAR COMO MD5 LA CLAVE
+								INSERT INTO `list_user` (`name_user`, `email_user`,`password_user`) VALUES (_name, _email ,md5(_password) ); -- INSERTAR DATOS Y GUARDAR COMO MD5 LA CLAVE
                                 -- Mensaje de exito
                                 SET _message = 'Se ha agregado satisfactoriamente!';
                             ELSE
@@ -36,20 +36,20 @@ BEGIN
 					ELSE
 						-- Error de email
 						SET _message = 'Este email ya se encuentra registrado.';
-                    END IF 
+                    END IF; 
                 ELSE
 					-- Error de email
-					SET _message = '';
+					SET _message = 'Ingrese un email valido!';
                 END IF;
             ELSE
 				-- Error de email
-				SET _message = '';
+				SET _message = 'Ingrese el email';
             END IF;
             
             
         ELSE
 			-- Error de nombre
-			SET _message = '';
+			SET _message = 'Ingrese un nombre';
         END IF;
     
     RETURN _message;
